9f70c1114cf5a14e56a08368988932da3f1c31bc
Elvina Diana <elvinadianalimartha@gmail.com>
2020-09-26 17:15:32 +0700

    food UI

--- app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/MainActivity.java
+++ app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/MainActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    //Kelompok PBP B , Wendy , Elvina , Wenny
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //Kelompok PBP B , Wendy , Elvina , Wenny
-
-
+        BottomNavigationView navView = findViewById(R.id.bottomNavigation);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
 
     }
 }
