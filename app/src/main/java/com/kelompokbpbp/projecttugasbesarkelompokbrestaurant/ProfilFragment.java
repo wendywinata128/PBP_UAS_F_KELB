2ea12a27d56cc21031b6f07f0ec0ff2707df32e6
wendy winata <wendywinata128@gmail.com>
2020-09-26 15:33:31 +0700

    commit new

---
+++ app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/ProfilFragment.java
@@ -0,0 +1,25 @@
+package com.kelompokbpbp.projecttugasbesarkelompokbrestaurant;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfilFragment extends Fragment {
+
+
+    public ProfilFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false);
+    }
+}
