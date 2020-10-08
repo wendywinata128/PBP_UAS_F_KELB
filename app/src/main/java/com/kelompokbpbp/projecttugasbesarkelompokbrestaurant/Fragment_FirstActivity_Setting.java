14debe42395157c5c355fb2794d277e48f0cd8e5
wendy winata <wendywinata128@gmail.com>
2020-10-08 23:45:32 +0700

    First Time Activity ( Location & Register )

--- app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/Fragment_FirstActivity_Setting.java
+++ app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/Fragment_FirstActivity_Setting.java
@@ -1,14 +1,25 @@
 package com.kelompokbpbp.projecttugasbesarkelompokbrestaurant;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import java.util.Objects;
 
 public class Fragment_FirstActivity_Setting extends Fragment {
+
+    private CheckBox cbNotification,cbLocation,cbCamera;
+
     public Fragment_FirstActivity_Setting() {
         // Required empty public constructor
     }
@@ -17,6 +28,28 @@ public class Fragment_FirstActivity_Setting extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__first_activity__setting, container, false);
+        View view = inflater.inflate(R.layout.fragment__first_activity__setting, container, false);
+
+        cbNotification = view.findViewById(R.id.cbNotification);
+        cbCamera = view.findViewById(R.id.cbCamera);
+        cbLocation = view.findViewById(R.id.cbLocation);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        cbLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+                }
+            }
+        });
+
     }
 }
