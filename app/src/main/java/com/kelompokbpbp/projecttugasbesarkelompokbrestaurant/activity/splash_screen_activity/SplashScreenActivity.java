fcb88bdb9900879145d0a9573929a2817cbe240e
wendy winata <wendywinata128@gmail.com>
2020-10-10 07:58:32 +0700

    anim

--- app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/activity/splash_screen_activity/SplashScreenActivity.java
+++ app/src/main/java/com/kelompokbpbp/projecttugasbesarkelompokbrestaurant/activity/splash_screen_activity/SplashScreenActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 import com.kelompokbpbp.projecttugasbesarkelompokbrestaurant.R;
 import com.kelompokbpbp.projecttugasbesarkelompokbrestaurant.activity.first_time_activity.FirstTimeActivity;
-import com.kelompokbpbp.projecttugasbesarkelompokbrestaurant.activity.main_activity.MainActivity;
 import com.kelompokbpbp.projecttugasbesarkelompokbrestaurant.database.AppPreference;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -20,12 +21,15 @@ public class SplashScreenActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        LinearLayout layoutContent = findViewById(R.id.layoutContent);
+
+        layoutContent.setAnimation(AnimationUtils.loadAnimation(SplashScreenActivity.this,R.anim.anim_splash_screen));
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 AppPreference appPreference = new AppPreference(SplashScreenActivity.this);
-
                 if(appPreference.getFirstRun()){
                     Intent intent = new Intent(SplashScreenActivity.this, FirstTimeActivity.class);
                     startActivity(intent);
